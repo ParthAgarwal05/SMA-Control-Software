b0838287abd0dde9b89dd58e3f0b72ac0b0bb45b
parth <parthagarwal9999@gmail.com>
2026-02-08 23:32:23 +0530

    Closed loop UI with global controls

--- software/SMAControlApp/Models/ActuatorChannel.cs
+++ software/SMAControlApp/Models/ActuatorChannel.cs
@@ -1,14 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace SMAControlApp.Models
 {
-    public class ActuatorChannel
+    public class ActuatorChannel : INotifyPropertyChanged
     {
-        public int ChannelId { get; set; }
-        public double DesiredDisplacement { get; set; }
-        public double CurrentDisplacement { get; set; }
-        public bool IsRunning { get; set; }
+        public int _channelId;
+        public double _desiredDisplacement;
+        public double _currentDisplacement;
+        public bool _isRunning;
+        public int ChannelId { 
+            get => _channelId;
+            set {
+                _channelId = value;
+                OnPropertyChanged();
+            }
+        }
+        public double DesiredDisplacement {
+            get => _desiredDisplacement;
+            set {
+                _desiredDisplacement = value;
+                OnPropertyChanged();
+            }
+        }
+        public double CurrentDisplacement { 
+            get => _currentDisplacement;
+            set {
+                _currentDisplacement = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsRunning {
+            get => _isRunning;
+            set {
+                _isRunning = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
--- software/SMAControlApp/ViewModels/ClosedLoopViewModel.cs
+++ software/SMAControlApp/ViewModels/ClosedLoopViewModel.cs
@@ -1,20 +1,47 @@
-﻿using System;
+﻿using SMAControlApp.Models;
+using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Collections.ObjectModel;
-using SMAControlApp.Models;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Drawing.Text;
+using System.Text;
 
 namespace SMAControlApp.ViewModels
 {
-    public class ClosedLoopViewModel
+    public class ClosedLoopViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        //private void Channel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        //{
+        //    if (e.PropertyName == nameof(ActuatorChannel.IsRunning))
+        //    {
+        //        OnPropertyChanged(nameof(IsAllRunning));
+        //    }
+        //}
+        //private void Channels_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        //{
+        //    if (e.NewItems != null)
+        //        foreach (ActuatorChannel c in e.NewItems)
+        //            c.PropertyChanged += Channel_PropertyChanged;
+
+        //    if (e.OldItems != null)
+        //        foreach (ActuatorChannel c in e.OldItems)
+        //            c.PropertyChanged -= Channel_PropertyChanged;
+        //}
+
         public ObservableCollection<ActuatorChannel> Channels { get; set; }
 
         public ClosedLoopViewModel()
         {
             Channels = new ObservableCollection<ActuatorChannel>();
 
-            for (int i = 1; i <= 35; i++)
+            for (int i = 1; i <= 5; i++)
             {
                 Channels.Add(new ActuatorChannel
                 {
@@ -24,7 +51,42 @@ namespace SMAControlApp.ViewModels
                     IsRunning = false
                 });
             }
+            //foreach (var c in Channels)
+            //    c.PropertyChanged += Channel_PropertyChanged;
+            //Channels.CollectionChanged += Channels_CollectionChanged;
+
         }
+        private bool _isAllRunning = false;
+        public bool IsAllRunning
+        {
+            get => _isAllRunning;
+            set
+            {
+                _isAllRunning = value;
+                foreach (var c in Channels)
+                    c.IsRunning = _isAllRunning;
+
+                OnPropertyChanged();
+            }
+        }
+        private double _globalDisplacement;
+        public double GlobalDisplacement
+        {
+            get => _globalDisplacement;
+            set
+            {
+                _globalDisplacement = value;
+                OnPropertyChanged();
+            }
+        }
+        public void ApplyDisplacementToAll()
+        {
+            foreach (var ch in Channels)
+            {
+                ch.DesiredDisplacement = GlobalDisplacement;
+            }
+        }
+
     }
 
 }
